13caa87ad20117e0f880f7aceef79de3effbc74c
EgorMIt <egor.mitrofanov@hotmail.com>
2022-09-11 12:35:58 +0300

    fix

--- src/main/java/com/example/shcool_yandex/application/domain/SystemItemRepository.java
+++ src/main/java/com/example/shcool_yandex/application/domain/SystemItemRepository.java
@@ -1,7 +1,6 @@
 package com.example.shcool_yandex.application.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
